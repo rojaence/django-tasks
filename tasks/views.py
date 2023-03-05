4424f0fcb365fc4bed2840fd7edd4cac8a07459a
Ronny Endara <rojaence@gmail.com>
2023-03-04 20:58:45 -0500

    ADD Signup template

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+def hello_world(request):
+    title = 'Hello world'
+    return render(request, 'signup.html', {
+        'title': title,
+        'form': UserCreationForm
+    })
