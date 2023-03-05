f776b54b4108c0d5bab69a116d01a9f723e534aa
Ronny Endara <rojaence@gmail.com>
2023-03-04 22:06:32 -0500

    ADD Error management in singup view

--- tasks/views.py
+++ tasks/views.py
@@ -1,11 +1,35 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 # Create your views here.
-def hello_world(request):
-    title = 'Hello world'
-    return render(request, 'signup.html', {
-        'title': title,
-        'form': UserCreationForm
-    })
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def signup(request):
+    if (request.method == 'GET'):
+        return render(request, 'signup.html', {
+            'form': UserCreationForm
+        })
+    if (request.method == 'POST'):
+        if request.POST['password1'] == request.POST['password2']:
+            # Register user
+            try:
+                new_user = User.objects.create_user(
+                    username=request.POST['username'], password=request.POST['password1'])
+                new_user.save()
+                return HttpResponse('User created successfully')
+            except:
+                return render(request, 'signup.html', {
+                    'form': UserCreationForm,
+                    'error': 'User name already exists'
+                })
+        else:
+            return render(request, 'signup.html', {
+                'form': UserCreationForm,
+                'error': 'Password do not match'
+            })
