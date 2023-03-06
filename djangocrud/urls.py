c72ecd98662cec064265d36141b530d8a05575fa
Ronny Endara <rojaence@gmail.com>
2023-03-05 21:57:30 -0500

    ADD Sigin and logout methods

--- djangocrud/urls.py
+++ djangocrud/urls.py
@@ -21,5 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('signup/', views.signup, name='signup'),
+    path('signin/', views.signin, name='signin'),
+    path('logout/', views.signout, name='logout'),
     path('tasks/', views.tasks, name='tasks'),
 ]
