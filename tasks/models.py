d4af602def83292415b5dae53b6531346a87c811
Ronny Endara <rojaence@gmail.com>
2023-03-05 23:52:55 -0500

    ADD Task model, favicon image, UPDATE admin in tasks

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Tasks(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=500, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    completed_at = models.DateTimeField(null=True)
+    important = models.BooleanField(default=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Task'
+        verbose_name_plural = 'Tasks'
+
+    def __str__(self):
+        return f'{self.title} - {self.user.username}'
